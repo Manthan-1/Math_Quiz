5f5f569defb63456bea9ef55fac99f3d9b591d38
Manthan Shah <shahmanthan1303@gmail.com>
2024-02-21 10:49:39 +0530

    Create math_quiz.sh

---
+++ math_quiz.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+    quiz() {
+      echo "Starting quiz..."
+      random1=$(($RANDOM / 100))
+      random2=$(($RANDOM / 100))
+      result=$(($random1 + $random2))
+  
+      echo "What is $random1 + $random2?"
+     read response
+     if [ "$response" == $result ]
+     then
+       echo "You got it."
+     else 
+       echo "That is incorrect."
+     fi
+    }
+
+   echo "Begin math quiz? (y/n)"
+   read response
+   if [ "$response" == "y" ] || [ "$response" == "Y" ]
+   then 
+     SECONDS=0
+     quiz
+     echo "The quiz took you $SECONDS seconds to complete."
+   else 
+     echo "Exiting..."
+   fi
